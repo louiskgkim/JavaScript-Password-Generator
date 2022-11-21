fafad51bb69146d45a6001e855dddf2490de06bb
louiskgkim <lkgk31@gmail.com>
2022-11-20 18:27:40 -0800

    adding mostly completed challenge files into github

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# JavaScript-Password-Generator
+# JavaScript Challenge: Password Generator
+
+1. [ Description ](#desc)
+2. [ Web Address ](#web-address)
+3. [ User Story & Acceptance Criteria ](#us&uc)
+4. [ License ](#license)
+
+
+<a name="desc"></a>
+## 1. Description
+
+This is the landing page for Password Generator:
+ 
+<!-- add image in github -->
+
+
+## 2. How to Get There
+
+Open your web browser and enter the following web address to access:
+
+```html
+<!-- add live link once published -->
+```
+<a name="us&uc"></a>
+## 3. User Story & Acceptance Criteria
+
+### User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+### Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+
+<a name="license"></a>
+## 4. License
+None
+
+<!-- Found this template of a README file online on Google, used previously on past challenge -->
---
+++ assets/script.js
@@ -0,0 +1,83 @@
+// Assignment Code (this was already provided)
+var generateBtn = document.querySelector("#generate");
+
+// Array of Possible Character's to Use
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var special = [")", "(", "}", "{", "]", "[", "~", "-", "_", ".", "/", "!", "#", "$", "^", "?", ":", "@", "%", "+"];
+
+
+// Function to prompt user options for password and minimum requirements
+function questions() {
+  var isValid = false;
+  do {
+    var length = prompt("Choose a password length between 8 and 128 characters");
+    var askNumbers = confirm("Do you want your password to include numbers?");
+    var askUpperCase = confirm("Do you want your password to include uppercase letters?");
+    var askLowerCase = confirm("Do you want your password to include lowercase letters?");
+    var askSpecial = confirm("Do you want your password to include special characters?");
+    var responses = {
+      length : length,
+      askNumbers : askNumbers,
+      askUpperCase : askUpperCase,
+      askLowerCase : askLowerCase,
+      askSpecial : askSpecial
+    }
+    if ((length < 8)||(length > 128))
+      alert("Please choose a number between 8 and 128.");
+      else if((!askNumbers)&&(!askUpperCase)&&(!askLowerCase)&&(!askSpecial))
+      alert("You must choose at least one type.");
+      else
+      isValid = true;
+  } while(!isValid);
+    return responses;
+}
+
+//  Function to merge the users response to generate a strong password
+function generatePassword() {
+  var passwordOptions = questions();
+  var possibleCombo = [];
+  var finalPassword = "";
+
+  if (passwordOptions.askNumbers) {
+    for (var i of numbers)
+    possibleCombo.push(i);
+  }
+  if (passwordOptions.askUpperCase) {
+    for (var i of upperCase)
+    possibleCombo.push(i);
+  }
+  if (passwordOptions.askLowerCase) {
+    for (var i of lowerCase)
+    possibleCombo.push(i);
+  }
+  if (passwordOptions.askSpecial) {
+    for (var i of special)
+    possibleCombo.push(i);
+  }
+
+  console.log(possibleCombo);
+
+  for (var i = 0; i < passwordOptions.length; i++) {
+    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)]; 
+  }
+
+  // Math.floor and Math.random was a good reminder from the mini project we did 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+  console.log(finalPassword);
+
+  return finalPassword;
+}
+
+// Write password to the #password input (this was already provided)
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button (this was already provided)
+generateBtn.addEventListener("click", writePassword);
