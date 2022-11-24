aba0eb6166341926dbff4e4896f1fefde669e71d
louiskgkim <lkgk31@gmail.com>
2022-11-23 19:51:54 -0800

    changed the array of characters with a split function to make the code cleaner

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 // Array of Possible Character's to Use
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var special = [")", "(", "}", "{", "]", "[", "~", "-", "_", ".", "/", "!", "#", "$", "^", "?", ":", "@", "%", "+"];
+var numbers = '0123456789'.split(
+  ''
+);
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split(
+  ''
+);
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
+  ''
+);
+var special = '{)}(]~[-_./!#$@^%?:%+)'.split(
+  ''
+);
 
+[")", "(", "}", "{", "]", "[", "~", "-", "_", ".", "/", "!", "#", "$", "^", "?", ":", "@", "%", "+"];
 
 // Function to prompt user options for password and minimum requirements
 function questions() {
